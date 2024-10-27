2e21d8af861c9b3a4e61e3c2b883edb4b6cf45a6
Danimexivasco <dcano9493@gmail.com>
2024-10-27 13:37:37 +0100

    Improve middleware logic

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,22 @@ import { type NextRequest, NextResponse } from "next/server";
 import { SESSION_COOKIE_NAME } from "@/constants";
 import { ROUTES } from "@/routes";
 
+const HOME_ROUTE_PATH = ROUTES.find((route) => route.name === "Home")?.path || "/";
+const LOGIN_ROUTE_PATH = ROUTES.find((route) => route.name === "SignIn")?.path || "/signIn";
+const protectedRoutes = ROUTES.filter((route) => route.protected)?.map((route) => route.path);
+const authRoutes = ROUTES.filter((route) => route.authRoute)?.map((route) => route.path);
 
 export function middleware(request: NextRequest) {
-  const HOME_ROUTE_PATH = ROUTES.find((route) => route.name === "Home")?.path || "/";
-  const LOGIN_ROUTE_PATH = ROUTES.find((route) => route.name === "Login")?.path || "/login";
-  const protectedRoutes = [ HOME_ROUTE_PATH ];
-  
   const session = request.cookies.get(SESSION_COOKIE_NAME)?.value || "";
   
   if (!session && protectedRoutes.includes(request.nextUrl.pathname)) {
     const absoluteURL = request.nextUrl.clone()
     absoluteURL.pathname = LOGIN_ROUTE_PATH
+    return NextResponse.redirect(absoluteURL)
+  }
+  if (session && authRoutes.includes(request.nextUrl.pathname)) {
+    const absoluteURL = request.nextUrl.clone()
+    absoluteURL.pathname = HOME_ROUTE_PATH
     return NextResponse.redirect(absoluteURL)
   }
 }

d5fdfcbc0eba82cded31e8789b5eb87b932e2c7e
Danimexivasco <dcano9493@gmail.com>
2024-10-27 13:38:23 +0100

    Add initial routes

--- routes.ts
+++ routes.ts
@@ -2,9 +2,30 @@ export const ROUTES = [
   {
     path: "/",
     name: "Home",
+    protected: true
   },
   {
-    path: "/login",
-    name: "Login",
+    path: "/signIn",
+    name: "SignIn",
+    authRoute: true
+  },
+  {
+    path: "/signUp",
+    name: "SignUp",
+    authRoute: true
+  },
+  {
+    path: "/products",
+    name: "Products",
+    protected: true
+  },
+  {
+    path: "/product/:id",
+    name: "ProductDetail",
+    protected: true
+  },
+  {
+    path: "/componentsPreview",
+    name: "ComponentsPreview",
   },
 ]

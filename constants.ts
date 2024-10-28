6f268d33044bea3328a4c168696240251b965125
Danimexivasco <dcano9493@gmail.com>
2024-10-28 20:04:04 +0100

    Add authentication and theme constants

--- constants.ts
+++ constants.ts
@@ -1,2 +1,21 @@
 export const SESSION_COOKIE_NAME = "user_session";
 export const ONE_DAY = 60 * 60 * 24;
+export const DEFAULT_THEME = "dark"
+
+export const AUTHENTICATION_FORM_INPUTS = [
+  { name: "email",
+    type: "email",
+    label:"Email",
+    placeholder: "Enter your email",
+    required: true },
+  { name: "password",
+    type: "password",
+    label:"Password",
+    placeholder: "Enter your password",
+    required: true },
+]
+
+export const AUTHENTICATION_FORM_INITIAL_STATE = {
+  email: "",
+  password: ""
+}

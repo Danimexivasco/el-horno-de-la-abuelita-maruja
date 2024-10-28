3b94f42e669410d5f4549f6da561edc62f7469df
Danimexivasco <dcano9493@gmail.com>
2024-10-28 20:01:20 +0100

    Auth logic adjustments

--- actions/authActions.ts
+++ actions/authActions.ts
@@ -1,28 +1,29 @@
 "use server";
 
-import { NextResponse } from "next/server";
 import { cookies  } from "next/headers";
-import { redirect } from "next/navigation";
 
 import { ROUTES } from "@/routes";
 import { ONE_DAY, SESSION_COOKIE_NAME } from "@/constants";
 
 const HOME_ROUTE_PATH = ROUTES.find((route) => route.path === "/")?.path || "/";
 export async function createSession(uid: string) {
-  const response = NextResponse.redirect(HOME_ROUTE_PATH);
-  response.cookies.set(SESSION_COOKIE_NAME, uid, {
+  const cookieStore = await cookies();
+  cookieStore.set({
+    name: SESSION_COOKIE_NAME,
+    value: uid,
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     maxAge: ONE_DAY,
     path: HOME_ROUTE_PATH,
+    sameSite: "lax",
   });
 
-  return response;
+  return { success: true };
 }
 
 export async function removeSession() {
   const cookieStore = await cookies();
   cookieStore.delete(SESSION_COOKIE_NAME);
 
-  redirect(HOME_ROUTE_PATH);
+  return { success: true };
 }
--- app/_libs/firebase/auth.ts
+++ app/_libs/firebase/auth.ts
@@ -9,6 +9,7 @@ import {
 
 import { firebaseAuth } from "./config";
 import { showMsg } from "@/utils/showMsg";
+import { createSession, removeSession } from "@/actions/authActions";
 
 export function onAuthStateChanged(callback: (authUser: User | null) => void) {
   return _onAuthStateChanged(firebaseAuth, callback);
@@ -18,21 +19,28 @@ export async function signInWithGoogle() {
   const provider = new GoogleAuthProvider();
 
   try {
-    const result = await signInWithPopup(firebaseAuth, provider);
+    const userCredential = await signInWithPopup(firebaseAuth, provider);
 
-    if (!result || !result.user) {
+    if (!userCredential || !userCredential.user) {
       throw new Error("Google sign in failed");
     }
-    return result.user.uid;
+    return userCredential.user.uid;
   } catch (error) {
     console.error("Error signing in with Google", error);
   }
 }
 
-export const createUserWithEmailAndPassword = async (email: string, password: string) => {
+export const signUpWithEmailAndPassword = async (formData: { email: string; password: string }) => {
+  const { email, password } = formData;
   if (!email || !password) throw new Error("Email and password are required");
   try {
-    await _createUserWithEmailAndPassword(firebaseAuth, email, password);
+    const userCredential = await _createUserWithEmailAndPassword(firebaseAuth, email, password);
+    if (!userCredential || !userCredential.user) {
+      throw new Error("Something failed during sign up");
+    }
+    await createSession(userCredential.user.uid)
+    
+    return userCredential.user.uid;
   } catch (error) {
     const message = (error instanceof Error) ? error.message : "An unexpected error occurred";
     showMsg(message, "error")
@@ -40,10 +48,16 @@ export const createUserWithEmailAndPassword = async (email: string, password: st
   }
 };
 
-export const signInWithEmailAndPassword = async (email: string, password: string) => {
+export const signInWithEmailAndPassword = async (formData: { email: string; password: string }) => {
+  const { email, password } = formData;
   if (!email || !password) throw new Error("Email and password are required");
   try {
-    return await _signInWithEmailAndPassword(firebaseAuth, email, password);
+    const userCredential = await _signInWithEmailAndPassword(firebaseAuth, email, password);
+    if (!userCredential || !userCredential.user) {
+      throw new Error("Something failed during sing in");
+    }
+    await createSession(userCredential.user.uid)
+    return userCredential.user.uid;
   } catch (error) {
     const message = (error instanceof Error) ? error.message : "An unexpected error occurred";
     showMsg(message, "error")
@@ -52,6 +66,7 @@ export const signInWithEmailAndPassword = async (email: string, password: string
 }
 export async function signOut() {
   try {
+    await removeSession()
     return await firebaseAuth.signOut()
   } catch (error) {
     const message = (error instanceof Error) ? error.message : "An unexpected error occurred";

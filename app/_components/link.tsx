fab84ad9a8a9f0e1f72e2d34cb3afd6f4bc42067
Danimexivasco <dcano9493@gmail.com>
2024-11-12 14:15:06 +0100

    Add custom props for link component

--- app/_components/link.tsx
+++ app/_components/link.tsx
@@ -7,16 +7,27 @@ type LinkProps = {
   external?: boolean;
   asButton?: boolean;
   className?: string;
+  props?: any
 }
 
-export default function Link({ href, children, external, asButton=false, className="" }: LinkProps) {
+export default function Link({ href, children, external, asButton=false, className="", ...props }: LinkProps) {
   const buttonClasses = asButton ? "button" : "underline"
   
-  if (external) return (<a href={href} className={combine("dark:text-cake-400 dark:active:text-cake-600 text-cake-600 active:text-cake-700", className, buttonClasses)} target="_blank">{children}</a>)
+  if (external) return (
+    <a
+      href={href}
+      className={combine("dark:text-cake-400 dark:active:text-cake-600 text-cake-600 active:text-cake-700", className, buttonClasses)}
+      target="_blank"
+      {...props}
+    >
+      {children}
+    </a>
+  )
   return (
     <NextLink
       href={href}
       className={combine("dark:text-cake-400 dark:active:text-cake-600 text-cake-600 active:text-cake-700", className, buttonClasses)}
+      {...props}
     >
       {children}
     </NextLink>

00a940079091b75219b3ad00254de21657e57e58
Danimexivasco <dcano9493@gmail.com>
2024-11-13 13:03:54 +0100

    Add LogOut icon

--- app/_icons/index.tsx
+++ app/_icons/index.tsx
@@ -204,3 +204,30 @@ export const GoogleIcon = ({ className }: IconProps) => (
     </g>
   </svg>
 )
+
+export const LogOutIcon = ({ className }: IconProps) => (
+  <svg
+    fill="currentColor"
+    height="200px"
+    width="200px"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 491.213 491.213"
+    xmlSpace="preserve"
+    className={className}
+  >
+    <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+    <g
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <g id="SVGRepo_iconCarrier">
+      <g>
+        <polygon points="146.213,3.107 146.213,175.607 176.213,175.607 176.213,33.107 461.213,33.107 461.213,458.107 176.213,458.107 176.213,315.607 146.213,315.607 146.213,488.107 491.213,488.107 491.213,3.107 " />
+        <polygon points="318.713,260.607 318.713,230.607 57.427,230.607 91.82,196.213 70.607,175 0,245.607 70.607,316.213 91.82,295 57.426,260.607 " />
+      </g>
+    </g>
+  </svg>
+)

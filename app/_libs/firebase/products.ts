82906abd94135147bc6b7858019b24a563a88d73
Danimexivasco <dcano9493@gmail.com>
2024-11-25 11:52:33 +0100

    Create products service

--- app/_libs/firebase/products.ts
+++ app/_libs/firebase/products.ts
@@ -1,9 +1,16 @@
-import { collection, doc, query, addDoc, deleteDoc, setDoc, QuerySnapshot, DocumentData } from "firebase/firestore";
-import { db } from "@/libs/firebase/config";
 import {
-  useCollectionData,
-  useDocumentData,
-} from "react-firebase-hooks/firestore";
+  collection,
+  doc,
+  query,
+  addDoc,
+  deleteDoc,
+  setDoc,
+  QuerySnapshot,
+  DocumentData,
+  getDoc
+} from "firebase/firestore";
+import { db } from "@/libs/firebase/config";
+import { useCollectionData, useDocumentData } from "react-firebase-hooks/firestore";
 import { Product } from "@/types";
 import { showMsg } from "@/utils/showMsg";
 
@@ -13,50 +20,53 @@ const _collection = collection(db, "products");
 export const useProductsData = (): [any, boolean, any, QuerySnapshot<DocumentData, DocumentData>] => {
   const _query = query(_collection);
 
-  const [ value, loading, error, snapshot ] = useCollectionData(_query);
+  const [value, loading, error, snapshot] = useCollectionData(_query);
 
-  return [ value, loading, error, snapshot! ];
+  return [value, loading, error, snapshot!];
 };
 
 // returns [value, loading, error, snapshot, reload]
 export const useSingleProductData = (id: string) => {
   const document = doc(db, "products", id);
 
-  const [ snapshot, loading, error ] = useDocumentData(document);
+  const [snapshot, loading, error] = useDocumentData(document);
 
-  return [ snapshot, loading, error ];
+  return [snapshot, loading, error];
 };
 
+export const getProduct = async (id: string): Promise<Product> => {
+  const productDoc = doc(db, "products", id);
+  const snapshot = await getDoc(productDoc);
+  return snapshot.data() as Product;
+};
 
 export const createProduct = async (data: Product) => {
   try {
     await addDoc(_collection, data);
-    showMsg("Product created", "success")
+    showMsg("Product created", "success");
   } catch {
-    showMsg("Something went wrong", "error")
+    showMsg("Something went wrong", "error");
   }
-}
-
+};
 
 export const updateProduct = async (id: string, data: Product | DocumentData) => {
   const productDoc = doc(db, "products", id);
 
   try {
     await setDoc(productDoc, data);
-    showMsg("Product updated", "success")
+    showMsg("Product updated", "success");
   } catch {
-    showMsg("Something went wrong", "error")
+    showMsg("Something went wrong", "error");
   }
-}
-
+};
 
 export const deleteProduct = async (id: string) => {
   const productDoc = doc(db, "products", id);
 
   try {
     await deleteDoc(productDoc);
-    showMsg("Product deleted", "success")
+    showMsg("Product deleted", "success");
   } catch {
-    showMsg("Something went wrong", "error")
+    showMsg("Something went wrong", "error");
   }
-}
+};

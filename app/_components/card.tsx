291bb3f63735f99eff8bc10c1391ddebc51c48f1
Danimexivasco <dcano9493@gmail.com>
2024-12-05 13:21:58 +0100

    Card style change

--- app/_components/card.tsx
+++ app/_components/card.tsx
@@ -4,21 +4,27 @@ import Image from "next/image";
 import { combine } from "../_utils/combineClassnames";
 import { LogoIcon } from "../_icons";
 import Tag from "./tag";
+import { getDiscountPrice } from "../_utils/getDiscountPrice";
 
 type CardProps = Omit<Product, "id" | "createdAt"> & {
   className?: string,
 };
 
-export default function Card({ name, description, category, price, image, className="" }: CardProps) {
+export default function Card({ name, description, category, price, image, onOffer, offerType, discountPercentage, multiplierAmount, className="" }: CardProps) {
   return (
     <li
       className={
         combine(
-          "flex flex-col gap-4 h-full w-full bg-cake-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border-2 dark:border-cake-400/80 border-cake-500/80 overflow-hidden",
+          "relative flex flex-col gap-4 h-full w-full bg-cake-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 shadow-xl overflow-hidden",
           className
         )
       }
     >
+      {onOffer === "yes" &&
+        <div className="absolute -top-1 -right-0.5 rotate-45 translate-x-1/4 translate-y-1/3 px-2 py-1 bg-cake-500 w-32 flex items-center justify-center">
+          <p className="text-2xl font-bold text-black">{offerType === "percentage" ? `${discountPercentage}%` : multiplierAmount}</p>
+        </div>
+      }
       <div
         className={
           combine(
@@ -39,14 +45,23 @@ export default function Card({ name, description, category, price, image, classN
           <LogoIcon className="w-1/4 h-auto opacity-60"/>
         }
       </div>
-      <div className="text-center px-6 pb-6 pt-2">
+      <div className="px-6 pb-6 pt-2">
         <Tag
           text={category}
           className="mb-4"
         />
         <Headline as="h2">{name}</Headline>
         <p className="line-clamp-1">{description}</p>
-        <p className="font-bold mt-4 text-xl">{price} €</p>
+        <p className="flex gap-2 items-center font-bold mt-4 text-xl">
+          {onOffer === "yes" && offerType === "percentage" ?
+            <>
+              <span className="text-3xl">{getDiscountPrice(price, discountPercentage ?? 0)}€</span>
+              <span className="font-normal line-through dark:text-red-400 text-red-500 transition-colors">{price}€</span>
+            </>
+            :
+            `${price}€`
+          }
+        </p>
       </div>
     </li>
   );

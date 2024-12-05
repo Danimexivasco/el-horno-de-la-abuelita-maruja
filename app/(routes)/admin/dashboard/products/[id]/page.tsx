5338f54c20799d4a08c67b763e821d1fccb7eb66
Danimexivasco <dcano9493@gmail.com>
2024-12-05 13:26:33 +0100

    Implement changes to edit product page

--- app/(routes)/admin/dashboard/products/[id]/page.tsx
+++ app/(routes)/admin/dashboard/products/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { Metadata } from "next";
-import Headline from "@/app/_components/headline";
-import { getProduct } from "@/app/_libs/firebase/products";
-import Image from "next/image";
+import ProductForm from "@/app/_components/forms/product";
 import Link from "@/app/_components/link";
-import { ADMIN_PRODUCTS_PATH } from "@/routes";
 import { BackIcon } from "@/app/_icons";
+import { getProduct } from "@/app/_libs/firebase/products";
+import { NEW_PRODUCT_FORM_INPUTS } from "@/constants";
+import { ADMIN_PRODUCTS_PATH } from "@/routes";
+import { Metadata } from "next";
 
 type ProductDetailProps = {
   params: {
@@ -28,48 +28,38 @@ export async function generateMetadata(
 export default async function ProductDetail({ params }: ProductDetailProps) {
 
   const { id } = await params;
-  const { name, description, category, price, image="", createdAt } = await getProduct(id);
+  const { name, description, category, price, image="", onOffer, offerType, discountPercentage, multiplierAmount, new: isNew } = await getProduct(id);
 
   return (
     <>
       <Link
         href={ADMIN_PRODUCTS_PATH}
-        className="mb-12 flex w-fit items-center gap-2 no-underline"
+        className="mb-10 flex w-fit items-center gap-2 no-underline"
       >
         <BackIcon className="w-6 h-6"/> {"Volver a los productos"}
       </Link>
-      {image &&
-        <Image
-          src={image}
-          alt={name}
-          width="500"
-          height="500"
-          quality={100}
-          className="w-1/2 h-auto aspect-video object-cover rounded-lg mb-4"
-        />
-      }
-      <Headline className="font-bold mb-8">{name}</Headline>
-      <ul className="grid gap-4">
-        {description &&
-          <li>
-            <p className="font-bold">Descripción:</p>
-            <p>{description}</p>
-          </li>
-        }
-        {category &&
-          <li>
-            <p className="font-bold">Categoría:</p>
-            <p>{category}</p>
-          </li>
-        }
-        {price &&
-          <li>
-            <p className="font-bold">Precio:</p>
-            <p>{price} €</p>
-          </li>
-        }
-      </ul>
-      {/* <p>{createdAt}</p> */}
+      <ProductForm
+        headline={name ?? "Editar producto"}
+        inputs={NEW_PRODUCT_FORM_INPUTS}
+        submitBtnText="Guardar Cambios"
+        redirectTo={ADMIN_PRODUCTS_PATH}
+        initialState={{
+          id,
+          name:               name,
+          description:        description,
+          category:           category,
+          image:              image,
+          // sizes?: Record<string, number>[] // Array of type: [{tamaño: "grande", precio: 14}]
+          price:              price, // Maybe we need to remove it and pass to sizes
+          onOffer:            onOffer,
+          offerType:          offerType,
+          discountPercentage: discountPercentage,
+          multiplierAmount:   multiplierAmount,
+          new:                isNew
+        }}
+        outterClassName="!flex"
+        fieldsContainerClassName="w-1/2 gap-8"
+      />
     </>
   );
 }

8c8218734e3deae6291ee615cfeef20602f56145
Danimexivasco <dcano9493@gmail.com>
2024-12-05 13:21:31 +0100

    Tag and ThemeSwitchButton style improvements

--- app/_components/tag.tsx
+++ app/_components/tag.tsx
@@ -1,14 +1,14 @@
-import { combine } from "../_utils/combineClassnames"
+import { combine } from "../_utils/combineClassnames";
 
 type TagProps = {
   text: string,
   className?: string
-}
+};
 
-export default function Tag({ text, className="" } : TagProps) {
+export default function Tag({ text, className="" }: TagProps) {
   return (
-    <div className={combine("dark:bg-cake-400 bg-cake-500 text-black font-bold px-2 py-1 rounded-md w-fit", className)}>
+    <div className={combine("outline outline-1 dark:outline-cake-400 outline-cake-500 dark:text-cake-400 text-cake-500 font-bold px-2 py-1 rounded-md w-fit", className)}>
       <p className="text-base">{text}</p>
     </div>
-  )
+  );
 }

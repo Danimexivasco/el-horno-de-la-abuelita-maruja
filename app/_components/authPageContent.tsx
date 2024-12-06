28c9ce5c7d8d31d4ee6824ccf6147f85701960af
Danimexivasco <dcano9493@gmail.com>
2024-12-06 10:10:42 +0100

    Handle firebase auth/popup-closed-by-user case and auth page style changes

--- app/_components/authPageContent.tsx
+++ app/_components/authPageContent.tsx
@@ -1,17 +1,17 @@
-import FullLogo from "@/components/fullLogo"
-import Container from "@/components/container"
-import AuthenticationCard from "@/components/authenticationCard"
-import { AuthenticationPages } from "@/types"
+import FullLogo from "@/components/fullLogo";
+import Container from "@/components/container";
+import AuthenticationCard from "@/components/authenticationCard";
+import { AuthenticationPages } from "@/types";
 
 type AuthPageContentProps = {
   type: AuthenticationPages
-}
+};
 
 export default function AuthPageContent({ type }: AuthPageContentProps) {
   return (
-    <Container className="flex flex-col md:flex-row items-center gap-12 md:gap-20 xl:gap-32">
+    <Container className="flex flex-col max-w-6xl md:flex-row items-center gap-12 md:gap-20 xl:gap-32">
       <FullLogo className="w-72 md:w-1/2"/>
       <AuthenticationCard type={type}/>
     </Container>
-  )
+  );
 }

de7305d70c45401460cb9e2ec20f982ac8677f54
Danimexivasco <dcano9493@gmail.com>
2024-12-10 13:00:33 +0100

    Add MobileMenuArrow icon

--- app/_icons/index.tsx
+++ app/_icons/index.tsx
@@ -313,3 +313,45 @@ export const TrashIcon = ({ className }: IconProps) => (
     </g>
   </svg>
 );
+
+export const MobileMenuArrow = ({ className }: IconProps) => (
+  <svg
+    viewBox="-4.5 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    fill="currentColor"
+    className={className}
+  >
+    <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+    <g
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <g id="SVGRepo_iconCarrier">
+      <title>{"arrow_right [#333]"}</title>
+      <desc>{"Created with Sketch."}</desc>
+      <defs />
+      <g
+        id="Page-1"
+        stroke="none"
+        strokeWidth={1}
+        fill="none"
+        fillRule="evenodd"
+      >
+        <g
+          id="Dribbble-Light-Preview"
+          transform="translate(-425.000000, -6679.000000)"
+          fill="currentColor"
+        >
+          <g id="icons" transform="translate(56.000000, 160.000000)">
+            <path
+              d="M370.39,6519 L369,6520.406 L377.261,6529.013 L376.38,6529.931 L376.385,6529.926 L369.045,6537.573 L370.414,6539 C372.443,6536.887 378.107,6530.986 380,6529.013 C378.594,6527.547 379.965,6528.976 370.39,6519"
+              id="arrow_right-[#333]"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);

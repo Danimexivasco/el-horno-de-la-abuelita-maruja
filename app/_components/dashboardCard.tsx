265b6f14634ff4606871d037685cb4f7aa7e2852
Danimexivasco <dcano9493@gmail.com>
2024-12-11 10:26:47 +0100

    Create DashboardCard component

---
+++ app/_components/dashboardCard.tsx
@@ -0,0 +1,26 @@
+import { combine } from "../_utils/combineClassnames";
+import Link from "./link";
+
+type DashboardCardProps = {
+    href: string;
+    children: React.ReactNode;
+    centered?: boolean
+    className?: string
+};
+
+export default function DashboardCard({ href, children, centered, className }: DashboardCardProps) {
+  return (
+    <li>
+      <Link
+        href={href}
+        className={combine(
+          "relative flex flex-col justify-center gap-4 p-6 h-full w-full bg-cake-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 shadow-xl overflow-hidden no-underline dark:!text-white !text-black transition-colors group-hover:shadow-lg dark:hover:shadow-cake-500/40 hover:shadow-black/30",
+          centered && "items-center",
+          className
+        )}
+      >
+        {children}
+      </Link>
+    </li>
+  );
+}

2905d1960f2735062daa6e8813bc8006ac5d1009
Danimexivasco <dcano9493@gmail.com>
2024-12-14 13:28:53 +0100

    Layout changes

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata, Viewport } from "next";
 // import localFont from "next/font/local";
-import { Toaster } from "react-hot-toast";
 import "@/app/globals.css";
-import Header from "../_components/header";
+import { Toaster } from "react-hot-toast";
 import Footer from "../_components/footer";
-import { headers } from "next/headers";
-import {
-  SIGN_IN_PATH,
-  SIGN_UP_PATH,
-  ADMIN_DASHBOARD_PATH,
-  ADMIN_PRODUCTS_PATH,
-  ADMIN_NEW_PRODUCT_PATH,
-  ADMIN_PRODUCT_DETAIL_PATH
-} from "@/routes";
+import Header from "../_components/header";
+import ThemeSwitchButton from "../_components/themeSwitchButton";
 
 // TODO: Check fonts
 // TODO: Change url when the website is on prod
@@ -46,36 +38,19 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
 
-  const backendLayoutPaths = [
-    SIGN_IN_PATH,
-    SIGN_UP_PATH,
-    ADMIN_DASHBOARD_PATH,
-    ADMIN_PRODUCTS_PATH,
-    ADMIN_NEW_PRODUCT_PATH,
-    ADMIN_PRODUCT_DETAIL_PATH
-  ];
-
-  const headersList = await headers();
-  const currentPath = headersList.get("x-current-path") ?? "";
-  const isBackendLayout = backendLayoutPaths.includes(currentPath);
-
   return (
     <html lang="es" className="dark">
       <body className="dark:bg-cake-900 bg-cake-100 dark:text-white transition-colors">
-        <main className="min-h-screen">
-          {isBackendLayout ? (
-            <>
-              <Toaster position="top-center" />
-              {children}
-            </>
-          ) : (
-            <>
-              <Header />
-              <Toaster position="top-center" />
-              {children}
-              <Footer />
-            </>
-          )}
+        <main className="flex flex-col min-h-screen">
+          <Toaster position="top-center" />
+          <Header />
+          <div className="flex-1">
+            {children}
+          </div>
+          <ThemeSwitchButton
+            className="fixed bottom-4 right-4 md:bottom-8 md:right-8"
+          />
+          <Footer />
         </main>
       </body>
     </html>

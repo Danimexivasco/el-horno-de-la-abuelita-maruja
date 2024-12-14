154f1958db80c364007f3d0e9e475f9421d43893
Danimexivasco <dcano9493@gmail.com>
2024-12-14 13:26:00 +0100

    Create getLoggedUser server function

--- actions/authActions.ts
+++ actions/authActions.ts
@@ -1,50 +1,75 @@
 "use server";
 
-import { cookies  } from "next/headers";
+import { cookies } from "next/headers";
 
 import { HOME_PATH } from "@/routes";
-import { ONE_DAY, SESSION_COOKIE_NAME, USER_CHECKED_COOKIE_NAME } from "@/constants";
+import {
+  ONE_DAY,
+  SESSION_COOKIE_NAME,
+  USER_CHECKED_COOKIE_NAME
+} from "@/constants";
+import { getActualUser } from "@/app/_libs/firebase/users";
 
 export async function createSession(uid: string) {
   const cookieStore = await cookies();
   cookieStore.set({
-    name: SESSION_COOKIE_NAME,
-    value: uid,
+    name:     SESSION_COOKIE_NAME,
+    value:    uid,
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    maxAge: ONE_DAY,
-    path: HOME_PATH,
-    sameSite: "lax",
+    secure:   process.env.NODE_ENV === "production",
+    maxAge:   ONE_DAY,
+    path:     HOME_PATH,
+    sameSite: "lax"
   });
 
-  return { success: true };
+  return {
+    success: true
+  };
 }
 
 export async function removeSession() {
   const cookieStore = await cookies();
   cookieStore.delete(SESSION_COOKIE_NAME);
 
-  return { success: true };
+  return {
+    success: true
+  };
 }
 
 export async function setAdminUserCheck() {
   const cookieStore = await cookies();
   cookieStore.set({
-    name: USER_CHECKED_COOKIE_NAME,
-    value: "true",
+    name:     USER_CHECKED_COOKIE_NAME,
+    value:    "true",
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    maxAge: ONE_DAY,
-    path: HOME_PATH,
-    sameSite: "lax",
+    secure:   process.env.NODE_ENV === "production",
+    maxAge:   ONE_DAY,
+    path:     HOME_PATH,
+    sameSite: "lax"
   });
 
-  return { success: true };
+  return {
+    success: true
+  };
 }
 
 export async function removeAdminUserCheck() {
   const cookieStore = await cookies();
   cookieStore.delete(USER_CHECKED_COOKIE_NAME);
 
-  return { success: true }
+  return {
+    success: true
+  };
 }
+
+export async function getLoggedUser (client=false) {
+  const cookieStore = await cookies();
+  const sessionId = cookieStore.get("user_session")?.value as string;
+
+  if (sessionId) {
+    const user = await getActualUser(sessionId);
+
+    return client ? JSON.stringify(user) : user;
+  }
+  return null;
+};

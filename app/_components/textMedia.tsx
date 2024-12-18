7ffbc04408638f0d5c17dbd65291eeefa9255485
Danimexivasco <dcano9493@gmail.com>
2024-12-18 13:07:03 +0100

    Create textMedia component

---
+++ app/_components/textMedia.tsx
@@ -0,0 +1,77 @@
+import ReactMarkdown from "react-markdown";
+import Container from "./container";
+import Headline from "./headline";
+import Image from "next/image";
+import React from "react";
+import { combine } from "../_utils/combineClassnames";
+
+type TextMediaProps = {
+    headline: string;
+    subHeadline: string;
+    text: string;
+    cta?: React.ReactNode
+    media: string;
+    mediaPosition?: "right" | "left"
+};
+
+export default function TextMedia({ headline, subHeadline, text, cta, media, mediaPosition="right" }: TextMediaProps) {
+  if (mediaPosition === "left") return (
+    <Container
+      as="section"
+      className="flex flex-col lg:flex-row items-center gap-8 lg:gap-16 px-0 lg:px-8 py-16 lg:py-8"
+    >
+      <div className="w-full lg:w-1/2 grid place-content-center">
+        <Image
+          src={media}
+          alt="media"
+          width={600}
+          height={600}
+          className="rounded-lg aspect-video object-cover shadow-lg shadow-black/30"
+        />
+      </div>
+      <div className="w-full lg:w-1/2">
+        <Headline as="h2">{headline}</Headline>
+        <Headline
+          as="h5"
+          empathized
+        >
+          {subHeadline}
+        </Headline>
+        <ReactMarkdown className={combine(Boolean(cta) && "mb-8")}>{text}</ReactMarkdown>
+        <div>
+          {cta && cta}
+        </div>
+      </div>
+    </Container>
+  );
+
+  return (
+    <Container
+      as="section"
+      className="flex flex-col lg:flex-row items-center gap-8 lg:gap-16 px-0 lg:px-8 py-16 lg:py-8"
+    >
+      <div className="w-full lg:w-1/2">
+        <Headline as="h2">{headline}</Headline>
+        <Headline
+          as="h5"
+          empathized
+        >
+          {subHeadline}
+        </Headline>
+        <ReactMarkdown className={combine(Boolean(cta) && "mb-8")}>{text}</ReactMarkdown>
+        <div>
+          {cta && cta}
+        </div>
+      </div>
+      <div className="w-full lg:w-1/2 grid place-content-center">
+        <Image
+          src={media}
+          alt="media"
+          width={600}
+          height={600}
+          className="rounded-lg aspect-video object-cover"
+        />
+      </div>
+    </Container>
+  );
+}

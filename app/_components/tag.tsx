47ddc68a79033ca76fc1d13e42df3f4552ddfbe0
Danimexivasco <dcano9493@gmail.com>
2024-12-20 13:02:14 +0100

    Change tag font family

--- app/_components/tag.tsx
+++ app/_components/tag.tsx
@@ -1,3 +1,4 @@
+import { sriracha } from "../_fonts";
 import { combine } from "../_utils/combineClassnames";
 
 type TagProps = {
@@ -7,7 +8,7 @@ type TagProps = {
 
 export default function Tag({ text, className="" }: TagProps) {
   return (
-    <div className={combine("outline outline-1 dark:outline-cake-400 outline-cake-500 dark:text-cake-400 text-cake-500 font-bold px-2 py-1 rounded-md w-fit", className)}>
+    <div className={combine("outline outline-1 dark:outline-cake-400 outline-cake-500 dark:text-cake-400 text-cake-500 font-bold px-2 py-1 rounded-md w-fit", sriracha.className, className)}>
       <p className="text-base">{text}</p>
     </div>
   );

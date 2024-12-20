ad18836a2cad9b578f69d48c85c9b7a1b4a4dcf4
Danimexivasco <dcano9493@gmail.com>
2024-12-20 21:02:08 +0100

    Create search component

---
+++ app/_components/search.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import Input from "./input";
+import { LensIcon } from "../_icons/lens";
+
+export default function Search() {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const currentParams = new URLSearchParams(searchParams.toString());
+
+    if (query) {
+      currentParams.set("search", query);
+    } else {
+      currentParams.delete("search");
+    }
+
+    router.replace(`?${currentParams.toString()}`);
+  }, [query, router, searchParams]);
+
+  useEffect(() => {
+    setQuery(searchParams.get("search") ?? "");
+  }, [searchParams]);
+
+  const handleQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <section className="grid place-items-end justify-items-center min-h-[15vh] dark:bg-cake-950 bg-cake-200/70 p-8">
+      <div className="w-full lg:w-2/4 flex justify-center items-center gap-4">
+        <LensIcon className="w-6 h-6 dark:text-cake-400 text-cake-600"/>
+        <Input
+          type="search"
+          name="search"
+          value={query}
+          placeholder="Busca entre nuestros productos"
+          onChange={handleQuery}
+          className="flex-1"
+        />
+      </div>
+
+    </section>
+  );
+}

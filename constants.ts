6953a01bccd7d6b6626e8f8590961bd5f6d74081
Danimexivasco <dcano9493@gmail.com>
2024-12-20 10:05:13 +0100

    Set default theme to light

--- constants.ts
+++ constants.ts
@@ -3,7 +3,7 @@ import { Product } from "./types";
 export const SESSION_COOKIE_NAME = "user_session";
 export const USER_CHECKED_COOKIE_NAME = "user_c";
 export const ONE_DAY = 60 * 60 * 24;
-export const DEFAULT_THEME = "dark";
+export const DEFAULT_THEME = "light";
 
 export const AUTHENTICATION_FORM_INPUTS = [
   {

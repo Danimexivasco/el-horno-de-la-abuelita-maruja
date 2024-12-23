45d67224fb6b0fa9e56140ac114b94214c9cfad8
Danimexivasco <dcano9493@gmail.com>
2024-12-23 17:44:09 +0100

    Create getFilters util function

---
+++ app/_utils/getFilters.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @stylistic/js/max-len */
+import { Product } from "@/types";
+import { FiltersState } from "../_components/filters";
+
+export const getFilters = (products: Product[]) => {
+
+  const filters: FiltersState = {
+    category:  [],
+    allergens: [],
+    priceFrom: 0,
+    priceTo:   0
+  };
+
+  products?.forEach((product: Product) => {
+
+    if (!filters.category?.includes(product.category)) {
+      filters.category?.push(product.category);
+    }
+
+    if (product.allergens?.some((allergen: string) => !filters.allergens?.includes(allergen))) {
+      filters.allergens?.push(...product.allergens);
+    }
+
+    if (product.multiPrice === "yes") {
+      const sortedVariants = product.variants?.sort((a, b) => a.value - b.value) ?? [];
+
+      const minVariant = sortedVariants[0];
+      const maxVariant = sortedVariants.pop();
+      const minPrice = minVariant ? minVariant.value : 0;
+      const maxPrice = maxVariant ? maxVariant.value : 0;
+
+      filters.priceFrom = filters.priceFrom !== 0 ? Math.min(filters.priceFrom, minPrice) : minPrice;
+      filters.priceTo = filters.priceTo !== 0 ? Math.max(filters.priceTo, maxPrice) : maxPrice;
+    } else {
+      filters.priceFrom = filters.priceFrom !== 0 ? Math.min(filters.priceFrom, product.price): product.price;
+      filters.priceTo = filters.priceTo !== 0 ? Math.max(filters.priceTo, product.price): product.price;
+    }
+  });
+
+  return filters;
+};

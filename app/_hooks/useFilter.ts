61acc91ded41c654c2b8571d541223ff4360486a
Danimexivasco <dcano9493@gmail.com>
2024-12-23 17:46:21 +0100

    Create useFilter custom hook

--- app/_hooks/useFilter.ts
+++ app/_hooks/useFilter.ts
@@ -1,6 +1,10 @@
+import { FILTER_PARAMS } from "@/constants";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 
-export default function useFilter(items: any[], query: string, fields: string[], debounceDelay = 100) {
+export default function useFilter(items: any[], targetFields: string[], debounceDelay = 100) {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("search") ?? "";
   const [filteredItems, setFilteredItems] = useState(items ?? []);
   const [debouncedValue, setDebouncedValue] = useState("");
   const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
@@ -16,15 +20,52 @@ export default function useFilter(items: any[], query: string, fields: string[],
   }, [query, debounceDelay]);
 
   useEffect(() => {
-    if (query) {
+    if (FILTER_PARAMS.some(param => searchParams.get(param))) {
       filterItems();
     } else {
       setFilteredItems(items);
     }
-  }, [items, debouncedValue]);
+  }, [items, searchParams, debouncedValue]);
 
   const filterItems = () => {
-    const filteredItems = items.filter(item => fields.some(field => item[field]?.toLowerCase().includes(query.toLowerCase())));
+    let filteredItems = items;
+    searchParams.forEach((value, key) => {
+      if (key === "priceFrom" || key === "priceTo") {
+        const priceFrom = parseFloat(searchParams.get("priceFrom") ?? "0");
+        const priceTo = parseFloat(searchParams.get("priceTo") ?? "Infinity");
+
+        filteredItems = filteredItems.filter((item) => {
+          const price = item.price ?? 0;
+          return price >= priceFrom && price <= priceTo;
+        });
+      } else if (key === "allergens") {
+        const allergens = value.split(",");
+
+        filteredItems = filteredItems.filter(
+          (item) => allergens.every((allergen) =>
+            !item.allergens?.includes(allergen.toLowerCase())
+          )
+        );
+      } else if (key === "search") {
+        filteredItems = filteredItems.filter((item) =>
+          targetFields.some((field) =>
+            item[field]?.toLowerCase().includes(value.toLowerCase())
+          )
+        );
+      } else if (key === "category") {
+        const categories = value.split(",");
+
+        filteredItems = filteredItems.filter((item) =>
+          categories.some((category) =>
+            item.category?.toLowerCase().includes(category.toLowerCase())
+          )
+        );
+      } else {
+        filteredItems = filteredItems.filter((item) =>
+          item[key]?.toLowerCase().includes(value.toLowerCase())
+        );
+      }
+    });
 
     return setFilteredItems(filteredItems);
   };

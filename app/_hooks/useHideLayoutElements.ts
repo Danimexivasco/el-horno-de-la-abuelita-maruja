e8fd0af49e2fe23babca978290220c3a2495d38c
Danimexivasco <dcano9493@gmail.com>
2024-12-23 14:49:39 +0100

    Create custom hooks to hide footer/header on admin pages

---
+++ app/_hooks/useHideLayoutElements.ts
@@ -0,0 +1,25 @@
+import { usePathname } from "next/navigation";
+import {
+  ADMIN_DASHBOARD_PATH,
+  ADMIN_NEW_PRODUCT_PATH,
+  ADMIN_PRODUCT_DETAIL_PATH,
+  ADMIN_PRODUCTS_PATH,
+  SIGN_IN_PATH,
+  SIGN_UP_PATH
+} from "@/routes";
+
+export default function useHideLayoutElements() {
+  const pathname = usePathname();
+
+  const pathsWithoutElements = [
+    SIGN_IN_PATH,
+    SIGN_UP_PATH,
+    ADMIN_DASHBOARD_PATH,
+    ADMIN_PRODUCTS_PATH,
+    ADMIN_NEW_PRODUCT_PATH,
+    ADMIN_PRODUCT_DETAIL_PATH.replace(":id", pathname.split("/").pop() ?? "")
+  ];
+
+  const hideElements = pathsWithoutElements.includes(pathname);
+  return hideElements;
+}

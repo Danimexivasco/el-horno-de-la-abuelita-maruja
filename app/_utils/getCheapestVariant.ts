4d15f2aee64be227eaa19ea9c0edf9b9be44f990
Danimexivasco <dcano9493@gmail.com>
2024-12-28 13:49:20 +0100

    Adjust getCheapestVariant function

--- app/_utils/getCheapestVariant.ts
+++ app/_utils/getCheapestVariant.ts
@@ -4,7 +4,7 @@ import { getDiscountPrice } from "./getDiscountPrice";
 export default function getCheapestVariant(variants: ProductVariant[]) {
   const sortedVariants = variants?.sort((a, b) => a.value - b.value) ?? [];
   const cheapestVariant = sortedVariants?.[0];
-  const cheapestVariantWithOffer = sortedVariants?.find(variant => variant.offerData?.onOffer === "yes" && variant.offerData.offerType === "percentage");
+  const cheapestVariantWithOffer = sortedVariants?.find(variant => variant.offerData?.onOffer === "yes" && variant.offerData?.offerType === "percentage");
   const variantWithOfferPrice = getDiscountPrice(cheapestVariantWithOffer?.value ?? 0, cheapestVariantWithOffer?.offerData?.discountPercentage ?? 0);
   const returningVariant = cheapestVariantWithOffer && variantWithOfferPrice <= cheapestVariant?.value ? cheapestVariantWithOffer : cheapestVariant;
 

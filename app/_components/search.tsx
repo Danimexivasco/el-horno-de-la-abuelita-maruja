06b579dcd302ab47e21622981acd623aacb1a8c7
Danimexivasco <dcano9493@gmail.com>
2024-12-28 21:18:18 +0100

    Adjust search input max width on desktop

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -64,7 +64,7 @@ export default function Search() {
   return (
     <section className="grid place-items-end justify-items-center min-h-[15vh] dark:bg-cake-950 bg-cake-200/70 p-8">
       <div
-        className="w-full lg:w-2/4 flex justify-center items-center bg-white rounded-full px-4 py-2 focus-within:ring-2 focus-within:ring-cake-600 shadow-md"
+        className="w-full lg:w-2/4 lg:max-w-2xl flex justify-center items-center bg-white rounded-full px-4 py-2 focus-within:ring-2 focus-within:ring-cake-600 shadow-md"
       >
         <LensIcon className="w-6 h-6 dark:text-cake-400 text-cake-600"/>
         <Input

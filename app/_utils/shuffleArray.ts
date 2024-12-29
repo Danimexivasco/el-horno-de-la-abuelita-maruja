02f28bbe299a804f48ad0d920a23a20f9b8a69f4
Danimexivasco <dcano9493@gmail.com>
2024-12-29 20:10:38 +0100

    Create shuffleArray function

---
+++ app/_utils/shuffleArray.ts
@@ -0,0 +1,9 @@
+import { Product } from "@/types";
+
+export function shuffleArray(arr: Product[]): Product[] {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}

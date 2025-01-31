1c5488f48682cd251c912fb1ba38aa6d709e83a4
Danimexivasco <dcano9493@gmail.com>
2025-01-31 09:58:50 +0100

    Add option for not having external link icon

--- app/_components/dashboardCard.tsx
+++ app/_components/dashboardCard.tsx
@@ -15,6 +15,7 @@ export default function DashboardCard({ href, children, centered, className }: D
       <Link
         href={href}
         external={href === HOME_PATH}
+        noExternalIcon
         className={combine(
           "relative flex flex-col justify-center gap-4 p-6 h-full w-full bg-cake-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 shadow-xl overflow-hidden no-underline dark:!text-white !text-black transition-colors group-hover:shadow-lg dark:hover:shadow-cake-500/40 hover:shadow-black/30",
           centered && "items-center",

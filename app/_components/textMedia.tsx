c8f8440bd15812b63ef9c0562502e015a59e69ba
Danimexivasco <dcano9493@gmail.com>
2025-02-10 12:27:07 +0100

    Extend textMedia component

--- app/_components/textMedia.tsx
+++ app/_components/textMedia.tsx
@@ -1,24 +1,28 @@
 import ReactMarkdown from "react-markdown";
 import Container from "./container";
-import Headline from "./headline";
+import Headline, { HeadlineProps } from "./headline";
 import Image from "next/image";
 import React from "react";
 import { combine } from "../_utils/combineClassnames";
 
 type TextMediaProps = {
     headline: string;
+    headlineAs?: HeadlineProps["as"]
+    subHeadlineAs?: HeadlineProps["as"]
     subHeadline: string;
     text: string;
     cta?: React.ReactNode
     media: string;
     mediaPosition?: "right" | "left"
+    mediaAspect?: "video" | "square"
+    className?: string
 };
 
-export default function TextMedia({ headline, subHeadline, text, cta, media, mediaPosition="right" }: TextMediaProps) {
+export default function TextMedia({ headline, subHeadline, headlineAs="h2", subHeadlineAs="h5", text, cta, media, mediaPosition="right", mediaAspect="video", className }: TextMediaProps) {
   if (mediaPosition === "left") return (
     <Container
       as="section"
-      className="flex flex-col lg:flex-row items-center gap-8 lg:gap-16 px-0 lg:px-8 py-16 lg:py-8"
+      className={combine("flex flex-col lg:flex-row items-center gap-8 lg:gap-16 px-0 lg:px-8 py-16 lg:py-8", className && className)}
     >
       <div className="w-full lg:w-1/2 grid place-content-center">
         <Image
@@ -26,13 +30,13 @@ export default function TextMedia({ headline, subHeadline, text, cta, media, med
           alt="media"
           width={600}
           height={600}
-          className="rounded-lg aspect-video object-cover shadow-lg shadow-black/30"
+          className={combine("rounded-lg object-cover shadow-lg shadow-black/30", mediaAspect === "square" ? "aspect-square" : "aspect-video")}
         />
       </div>
       <div className="w-full lg:w-1/2">
-        <Headline as="h2">{headline}</Headline>
+        <Headline as={headlineAs}>{headline}</Headline>
         <Headline
-          as="h5"
+          as={subHeadlineAs}
           empathized
           className="mb-6"
         >
@@ -49,12 +53,12 @@ export default function TextMedia({ headline, subHeadline, text, cta, media, med
   return (
     <Container
       as="section"
-      className="flex flex-col lg:flex-row items-center gap-8 lg:gap-16 px-0 lg:px-8 py-16 lg:py-8"
+      className={combine("flex flex-col lg:flex-row items-center gap-8 lg:gap-16 px-0 lg:px-8 py-16 lg:py-8", className && className)}
     >
       <div className="w-full lg:w-1/2">
-        <Headline as="h2">{headline}</Headline>
+        <Headline as={headlineAs}>{headline}</Headline>
         <Headline
-          as="h5"
+          as={subHeadlineAs}
           empathized
           className="mb-6"
         >
@@ -71,7 +75,7 @@ export default function TextMedia({ headline, subHeadline, text, cta, media, med
           alt="media"
           width={600}
           height={600}
-          className="rounded-lg aspect-video object-cover"
+          className={combine("rounded-lg object-cover", mediaAspect === "square" ? "aspect-square" : "aspect-video")}
         />
       </div>
     </Container>

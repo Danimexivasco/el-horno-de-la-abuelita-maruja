5a2b296dab81d872e70d0b226b2a62c11a39e246
Danimexivasco <dcano9493@gmail.com>
2025-02-11 13:53:56 +0100

    Fix for SSR using leaflet

--- app/_components/map.tsx
+++ app/_components/map.tsx
@@ -1,24 +1,57 @@
 "use client";
 
-import L from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
 
-const DefaultIcon = L.icon({
-  iconUrl:      "/marker-icon.png",
-  shadowUrl:    "/marker-shadow.png",
-  iconSize:     [25, 41],
-  shadowSize:   [41, 41],
-  iconAnchor:   [12, 41],
-  shadowAnchor: [12, 41],
-  popupAnchor:  [1, -34]
-});
-
-L.Marker.prototype.options.icon = DefaultIcon;
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.MapContainer),
+  {
+    ssr: false
+  }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.TileLayer),
+  {
+    ssr: false
+  }
+);
+const Marker = dynamic(
+  () => import("react-leaflet").then((mod) => mod.Marker),
+  {
+    ssr: false
+  }
+);
+const Popup = dynamic(
+  () => import("react-leaflet").then((mod) => mod.Popup),
+  {
+    ssr: false
+  }
+);
 
 export default function MapComponent() {
-
+  const [L, setL] = useState<any>(null);
   const position = [38.360387, -2.100832];
+
+  useEffect(() => {
+    import("leaflet").then((leaflet) => {
+      setL(leaflet);
+    });
+  }, []);
+
+  if (!L) return null;
+
+  const DefaultIcon = L.icon({
+    iconUrl:      "/marker-icon.png",
+    shadowUrl:    "/marker-shadow.png",
+    iconSize:     [25, 41],
+    shadowSize:   [41, 41],
+    iconAnchor:   [12, 41],
+    shadowAnchor: [12, 41],
+    popupAnchor:  [1, -34]
+  });
+
+  L.Marker.prototype.options.icon = DefaultIcon;
 
   return (
     <div className="h-[50svh] lg:h-[300px]">

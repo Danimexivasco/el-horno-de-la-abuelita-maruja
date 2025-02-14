efb7642b696dfb65608e8e1da6c8eac6dc15011d
Danimexivasco <dcano9493@gmail.com>
2025-02-14 11:49:26 +0100

    Improve getPrices function

--- app/_utils/getPrices.ts
+++ app/_utils/getPrices.ts
@@ -2,14 +2,23 @@
 import { Product, ProductVariant } from "@/types";
 import { formatNumber } from "./formatNumber";
 
+const roundNumber = (number: number) => {
+  if (!number) throw new Error("No se ha pasado un parametro");
+  if (typeof number !== "number") throw new Error("No se ha pasado un numero");
+
+  return Math.round(number * 100) / 100;
+};
+
 export const getPrices = (product: Partial<Product>, quantity: number = 1, variant: Partial<ProductVariant> | null = null) => {
   if (!product) throw new Error("No se ha pasado un producto");
+  if (quantity <= 0) throw new Error("La cantidad no puede ser negativa");
+  if (variant && !variant?.id) throw new Error("No se ha pasado un variante");
 
   const { multiPrice, price, variants, onOffer, offerType, discountPercentage, multiplierAmount } = product;
 
   if ((multiPrice !== "yes" || !multiPrice) && onOffer === "yes" && offerType === "percentage") return {
     base:     price && Number(price),
-    offer:    price && discountPercentage && Number(price - (price * discountPercentage) / 100),
+    offer:    price && discountPercentage && roundNumber(Number(price - (price * discountPercentage) / 100)),
     discount: {
       type:  "percentage",
       label: discountPercentage && formatNumber(discountPercentage, "percent")
@@ -22,7 +31,7 @@ export const getPrices = (product: Partial<Product>, quantity: number = 1, varia
 
     return {
       base:     Number(product.price),
-      offer:    product.price && Number((effectiveQuantity * product.price) / quantity),
+      offer:    product.price && roundNumber(Number((effectiveQuantity * product.price) / quantity)),
       discount: {
         type:  "multiplier",
         label: multiplierAmount
@@ -42,7 +51,7 @@ export const getPrices = (product: Partial<Product>, quantity: number = 1, varia
     if (variant) {
       return {
         base:     Number(variant.value),
-        offer:    variant?.value && variant?.offerData?.discountPercentage && Number(variant?.value - (variant?.value * variant?.offerData?.discountPercentage) / 100),
+        offer:    variant?.value && variant?.offerData?.discountPercentage && roundNumber(Number(variant?.value - (variant?.value * variant?.offerData?.discountPercentage) / 100)),
         discount: {
           type:  "percentage",
           label: variant?.offerData?.discountPercentage && formatNumber(variant?.offerData?.discountPercentage, "percent")
@@ -55,9 +64,10 @@ export const getPrices = (product: Partial<Product>, quantity: number = 1, varia
     if (variant) {
       const [before, after] = variant?.offerData?.multiplierAmount?.split("x") ?? [];
       const effectiveQuantity = Math.floor(quantity / Number(before)) * Number(after) + (quantity % Number(before));
+
       return {
         base:     Number(variant.value),
-        offer:    variant.value && Number((effectiveQuantity * variant.value) / quantity),
+        offer:    variant.value && roundNumber(Number((effectiveQuantity * variant.value) / quantity)),
         discount: {
           type:  "multiplier",
           label: variant?.offerData?.multiplierAmount

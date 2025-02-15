282af4c9c18e145739403ee1e9c2ff2d1fdfe69a
Danimexivasco <dcano9493@gmail.com>
2025-02-15 18:11:25 +0100

    Make counter input readOnly

--- app/_components/counter.tsx
+++ app/_components/counter.tsx
@@ -24,6 +24,7 @@ export default function Counter({ value, setValue }: CounterProps) {
         step={1}
         max={MAXIMUM_PRODUCTS_PURCHASE}
         onChange={(e) => setValue(Number(e.target.value))}
+        readOnly
         className="text-center !w-fit rounded-none ring-0 focus:ring-0"
       />
       <Button

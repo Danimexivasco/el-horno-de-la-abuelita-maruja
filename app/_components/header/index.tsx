a579c248ca8a6c172d7f3a712bc908776dfd94dd
Danimexivasco <dcano9493@gmail.com>
2025-02-17 14:12:36 +0100

    Fix for mt on layouts without header

--- app/_components/header/index.tsx
+++ app/_components/header/index.tsx
@@ -23,25 +23,32 @@ export default function Header({ user }: HeaderProps) {
   const navRoutes = ROUTES.filter(route => route.isNavRoute && !route.protected);
 
   return (
-    <header
-      className={combine(
-        "fixed w-full top-0 left-0 z-50 lg:py-4 lg:px-8 flex items-center min-h-16 lg:h-36 bg-opacity-95 dark:bg-cake-950 bg-cake-200 transition-all ease-linear duration-200",
-        (scrollY > 0) && "glass !bg-opacity-80 shadow-md shadow-black/10",
-        hideHeader && "hidden"
-      )}
-    >
-      <DesktopHeader
-        navRoutes={navRoutes}
-        activePathname={pathname}
-        user={parsedUser}
-        className="hidden lg:grid"
-      />
-      <MobileHeader
-        navRoutes={navRoutes}
-        activePathname={pathname}
-        user={parsedUser}
-        className="flex lg:hidden"
-      />
-    </header>
+    <>
+      <header
+        className={combine(
+          "fixed w-full top-0 left-0 z-50 lg:py-4 lg:px-8 flex items-center min-h-16 lg:h-36 bg-opacity-95 dark:bg-cake-950 bg-cake-200 transition-all ease-linear duration-200",
+          (scrollY > 0) && "glass !bg-opacity-80 shadow-md shadow-black/10",
+          hideHeader && "hidden"
+        )}
+      >
+        <DesktopHeader
+          navRoutes={navRoutes}
+          activePathname={pathname}
+          user={parsedUser}
+          className="hidden lg:grid"
+        />
+        <MobileHeader
+          navRoutes={navRoutes}
+          activePathname={pathname}
+          user={parsedUser}
+          className="flex lg:hidden"
+        />
+      </header>
+      {
+        !hideHeader ? (
+          <div className="mt-16 lg:mt-36"></div>
+        ) : null
+      }
+    </>
   );
 }

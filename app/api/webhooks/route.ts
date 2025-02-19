18b91d097a99007e498f6fdb13db7ff682f4ee50
Danimexivasco <dcano9493@gmail.com>
2025-02-19 12:43:29 +0100

    Fix TS errors

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,55 +1,64 @@
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { stripe } from "@/app/_libs/stripe";
+import Stripe from "stripe";
 
 export async function POST(req: Request) {
-  let event;
+  let event: Stripe.Event | null = null;
 
   try {
+    const stripeSignature = (await headers()).get("stripe-signature");
+    if (stripeSignature === null) {
+      throw new Error("Stripe signature");
+    }
     event = stripe.webhooks.constructEvent(
       await req.text(),
-      (await headers()).get("stripe-signature"),
-      process.env.STRIPE_WEBHOOK_SECRET
+      stripeSignature,
+      process.env.STRIPE_WEBHOOK_SECRET ?? ""
     );
-  } catch (err) {
-    const errorMessage = err.message;
-    // On error, log and return the error message.
-    if (err) console.log(err);
-    console.log(`Error message: ${errorMessage}`);
-    return NextResponse.json(
-      {
-        message: `Webhook Error: ${errorMessage}`
-      },
-      {
-        status: 400
-      }
-    );
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      const errorMessage = err.message;
+      // On error, log and return the error message.
+      if (err) console.log(err);
+      console.log(`Error message: ${errorMessage}`);
+      return NextResponse.json(
+        {
+          message: `Webhook Error: ${errorMessage}`
+        },
+        {
+          status: 400
+        }
+      );
+    }
   }
 
   const permittedEvents = ["payment_intent.succeeded"];
 
-  if (permittedEvents.includes(event.type)) {
-    let data;
+  if (event) {
+    if (permittedEvents.includes(event.type)) {
+      let data;
 
-    try {
-      switch (event.type) {
-      case "payment_intent.succeeded":
-        data = event.data.object;
-        console.log(`Payment status: ${data.status}`);
-        break;
-      default:
-        throw new Error(`Unhandled event: ${event.type}`);
-      }
-    } catch (error) {
-      console.log(error);
-      return NextResponse.json(
-        {
-          message: "Webhook handler failed"
-        },
-        {
-          status: 500
+      try {
+        switch (event.type) {
+        case "payment_intent.succeeded":
+          data = event.data.object;
+          console.log(`Payment status: ${data.status}`);
+          break;
+        default:
+          throw new Error(`Unhandled event: ${event.type}`);
         }
-      );
+      } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+          {
+            message: "Webhook handler failed"
+          },
+          {
+            status: 500
+          }
+        );
+      }
     }
   }
   // Return a response to acknowledge receipt of the event.

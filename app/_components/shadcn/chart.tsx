8a9de4b0bedb79d98f7922978e7dafee2d417a0f
Danimexivasco <dcano9493@gmail.com>
2025-03-01 14:21:14 +0100

    Fix vercel build

--- app/_components/shadcn/chart.tsx
+++ app/_components/shadcn/chart.tsx
@@ -1,6 +1,17 @@
 "use client";
 
-import * as React from "react";
+// import * as React from "react";
+import {
+  ComponentProps,
+  ComponentType,
+  createContext,
+  CSSProperties,
+  forwardRef,
+  ReactNode,
+  useContext,
+  useId,
+  useMemo
+} from "react";
 import * as RechartsPrimitive from "recharts";
 
 import { cn } from "@/libs/utils";
@@ -14,8 +25,8 @@ const THEMES = {
 export type ChartConfig = {
   // eslint-disable-next-line
   [k in string]: {
-    label?: React.ReactNode
-    icon?: React.ComponentType
+    label?: ReactNode
+    icon?: ComponentType
   } & (
     | { color?: string; theme?: never }
     | { color?: never; theme: Record<keyof typeof THEMES, string> }
@@ -26,10 +37,10 @@ type ChartContextProps = {
   config: ChartConfig
 };
 
-const ChartContext = React.createContext<ChartContextProps | null>(null);
+const ChartContext = createContext<ChartContextProps | null>(null);
 
 function useChart() {
-  const context = React.useContext(ChartContext);
+  const context = useContext(ChartContext);
 
   if (!context) {
     throw new Error("useChart must be used within a <ChartContainer />");
@@ -38,16 +49,16 @@ function useChart() {
   return context;
 }
 
-const ChartContainer = React.forwardRef<
+const ChartContainer = forwardRef<
   HTMLDivElement,
-  React.ComponentProps<"div"> & {
+  ComponentProps<"div"> & {
     config: ChartConfig
-    children: React.ComponentProps<
+    children: ComponentProps<
       typeof RechartsPrimitive.ResponsiveContainer
     >["children"]
   }
 >(({ id, className, children, config, ...props }, ref) => {
-  const uniqueId = React.useId();
+  const uniqueId = useId();
   const chartId = `chart-${id || uniqueId.replace(/:/g, "")}`;
 
   return (
@@ -113,10 +124,10 @@ ${colorConfig
 
 const ChartTooltip = RechartsPrimitive.Tooltip;
 
-const ChartTooltipContent = React.forwardRef<
+const ChartTooltipContent = forwardRef<
   HTMLDivElement,
-  React.ComponentProps<typeof RechartsPrimitive.Tooltip> &
-    React.ComponentProps<"div"> & {
+  ComponentProps<typeof RechartsPrimitive.Tooltip> &
+    ComponentProps<"div"> & {
       hideLabel?: boolean
       hideIndicator?: boolean
       indicator?: "line" | "dot" | "dashed"
@@ -144,7 +155,7 @@ const ChartTooltipContent = React.forwardRef<
   ) => {
     const { config } = useChart();
 
-    const tooltipLabel = React.useMemo(() => {
+    const tooltipLabel = useMemo(() => {
       if (hideLabel || !payload?.length) {
         return null;
       }
@@ -232,7 +243,7 @@ const ChartTooltipContent = React.forwardRef<
                             {
                               "--color-bg":     indicatorColor,
                               "--color-border": indicatorColor
-                            } as React.CSSProperties
+                            } as CSSProperties
                           }
                         />
                       )
@@ -269,9 +280,9 @@ ChartTooltipContent.displayName = "ChartTooltip";
 
 const ChartLegend = RechartsPrimitive.Legend;
 
-const ChartLegendContent = React.forwardRef<
+const ChartLegendContent = forwardRef<
   HTMLDivElement,
-  React.ComponentProps<"div"> &
+  ComponentProps<"div"> &
     Pick<RechartsPrimitive.LegendProps, "payload" | "verticalAlign"> & {
       hideIcon?: boolean
       nameKey?: string
